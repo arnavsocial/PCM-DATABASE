157256a3d5b06c0e44179d6a365e637e75d6e64c
Arnav <256329715+arnavsocial@users.noreply.github.com>
2026-03-10 17:11:00 +0530

    Create OtherExams.js

---
+++ OtherExams.js
@@ -0,0 +1,8 @@
+const initialOtherExams = [
+    { id: 1, name: "NDA & NA (I) 2026", regStart: "Dec 20, 2025", regEnd: "Jan 09, 2026", examDate: "Apr 19, 2026", link: "upsc.gov.in" },
+    { id: 2, name: "NEET UG 2026", regStart: "Feb 09, 2026", regEnd: "Mar 09, 2026", examDate: "May 03, 2026", link: "neet.nta.nic.in" },
+    { id: 3, name: "UCEED 2026 (Design)", regStart: "Oct 03, 2025", regEnd: "Nov 13, 2025", examDate: "Jan 18, 2026", link: "uceed.iitb.ac.in" },
+    { id: 4, name: "CLAT 2026 (Law)", regStart: "Jul 15, 2025", regEnd: "Nov 15, 2025", examDate: "Dec 07, 2025", link: "consortiumofnlus.ac.in" },
+    { id: 5, name: "NIFT 2026 (Fashion)", regStart: "Nov 20, 2025", regEnd: "Jan 03, 2026", examDate: "Feb 05, 2026", link: "nift.ac.in" },
+    { id: 6, name: "CUET UG (General Test)", regStart: "Feb 09, 2026", regEnd: "Mar 31, 2026", examDate: "May 15-31, 2026", link: "cuet.samarth.ac.in" }
+];
