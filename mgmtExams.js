baebd89c9f5c806f4b47e09d5c4fce6faa697d7c
Arnav <256329715+arnavsocial@users.noreply.github.com>
2026-03-31 22:50:18 +0530

    Update mgmtExams.js

--- mgmtExams.js
+++ mgmtExams.js
@@ -1,11 +1,27 @@
 const initialMgmtExams = [
-    { id: 1, name: "CUET UG (BBA)", regStart: "Feb 09, 2026", regEnd: "Mar 31, 2026", examDate: "May 2026", link: "cuet.samarth.ac.in" },
-    { id: 2, name: "IPMAT Indore", regStart: "Feb 14, 2026", regEnd: "Mar 26, 2026", examDate: "May 2026", link: "iimidr.ac.in" },
-    { id: 3, name: "SET (Symbiosis)", regStart: "Dec 15, 2025", regEnd: "Apr 12, 2026", examDate: "May 2026", link: "set-test.org" },
-    { id: 4, name: "NPAT (NMIMS)", regStart: "Dec 01, 2025", regEnd: "May 20, 2026", examDate: "Jan-May 26", link: "npat.in" },
-    { id: 5, name: "Christ ET", regStart: "Jan 05, 2026", regEnd: "Apr 20, 2026", examDate: "May 2026", link: "christuniversity.in" },
-    { id: 6, name: "IPU CET (Mgmt)", regStart: "Feb 02, 2026", regEnd: "Mar 31, 2026", examDate: "May 2026", link: "ipu.ac.in" },
-    { id: 7, name: "JIPMAT 2026", regStart: "February 2026", regEnd: "March 2026", examDate: "April 2026", link: "jipmat.nta.ac.in" },
-    { id: 8, name: "IPMAT Rohtak 2026", regStart: "06 Feb ' 26", regEnd: "06 Apr ' 26", examDate: "10 May ' 26", link: "iimrohtak.ac.in" },
-    { id: 9, name: "IPMAT Rohtak (Result)", regStart: "N/A", regEnd: "N/A", examDate: "May ' 26 (Tentative)", link: "iimrohtak.ac.in" }
+  // National & Premier Institutes
+  { id: 1, name: "CUET UG (BBA/BMS)", regStart: "Feb 09, 2026", regEnd: "Feb 26, 2026", examDate: "May 11 - May 31, 2026", link: "cuet.nta.nic.in" },
+  { id: 2, name: "IPMAT Indore", regStart: "Feb 02, 2026", regEnd: "Mar 17, 2026", examDate: "May 04, 2026", link: "iimidr.ac.in" },
+  { id: 3, name: "IPMAT Rohtak", regStart: "Feb 06, 2026", regEnd: "Apr 06, 2026", examDate: "May 10, 2026", link: "iimrohtak.ac.in" },
+  { id: 4, name: "JIPMAT (IIM Jammu/Bodh Gaya)", regStart: "Feb 2026", regEnd: "Mar 29, 2026", examDate: "May 2026", link: "jipmat.nta.ac.in" },
+  
+  // Top Private University Exams
+  { id: 5, name: "SET (Symbiosis)", regStart: "Dec 2025", regEnd: "Apr 10, 2026", examDate: "May 02 & May 10, 2026", link: "set-test.org" },
+  { id: 6, name: "NPAT (NMIMS)", regStart: "Jan 28, 2026", regEnd: "May 26, 2026", examDate: "Feb 10 - Jun 01, 2026", link: "npat.nmims.edu" },
+  { id: 7, name: "Christ ET (CUET)", regStart: "Dec 18, 2025", regEnd: "Apr 14, 2026", examDate: "May 2026", link: "christuniversity.in" },
+  { id: 8, name: "CGCUET", regStart: "Nov 01, 2025", regEnd: "Mar 30, 2026", examDate: "Apr-May 2026", link: "cgc.ac.in" },
+  { id: 9, name: "FEAT (FLAME University)", regStart: "Jan 2026", regEnd: "Apr 01, 2026", examDate: "Mar 28 - Apr 01, 2026", link: "flame.edu.in" },
+  
+  // State & Other Prominent Exams
+  { id: 10, name: "IPU CET (BBA)", regStart: "Feb 02, 2026", regEnd: "Mar 31, 2026", examDate: "May 2026", link: "ipu.ac.in" },
+  { id: 11, name: "AIMA UGAT", regStart: "Jan 10, 2026", regEnd: "Jun 06, 2026", examDate: "Jun 09, 2026", link: "aima.in" },
+  { id: 12, name: "MAH-BBA/BCA CET", regStart: "Feb 2026", regEnd: "Mar 2026", examDate: "Apr 2026", link: "cetcell.mahacet.org" }, 
+  { id: 13, name: "BUMAT (Bharati Vidyapeeth)", regStart: "Nov 19, 2025", regEnd: "Jun 05, 2026", examDate: "Jun 2026", link: "bvuniversity.edu.in" },
+  { id: 14, name: "JIIT UGET", regStart: "Feb 28, 2026", regEnd: "Apr 30, 2026", examDate: "May 2026", link: "jiit.ac.in" },
+  { id: 15, name: "UPESMET-UG", regStart: "Nov 01, 2025", regEnd: "Mar 30, 2026", examDate: "Apr 2026", link: "upes.ac.in" },
+  { id: 16, name: "KIITEE (BBA)", regStart: "Nov 10, 2025", regEnd: "Mar 20, 2026", examDate: "Apr 2026", link: "kiitee.kiit.ac.in" },
+  { id: 17, name: "JET (Jain University)", regStart: "Nov 20, 2025", regEnd: "Apr 10, 2026", examDate: "Apr-May 2026", link: "jainuniversity.ac.in" },
+  { id: 18, name: "SRMJEEM UG (SRM)", regStart: "Nov 15, 2025", regEnd: "Apr 15, 2026", examDate: "Apr-May 2026", link: "srmist.edu.in" },
+  { id: 19, name: "Lucknow University UGPET", regStart: "Apr 01, 2026", regEnd: "May 31, 2026", examDate: "Jul 2026", link: "lkouniv.ac.in" },
+  { id: 20, name: "GITAM GAT (BBA)", regStart: "Nov 01, 2025", regEnd: "Mar 30, 2026", examDate: "Apr 2026", link: "gat.gitam.edu" }
 ];
